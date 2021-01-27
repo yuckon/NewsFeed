136d171150ba386fc7236b1f3b17f822b78f836c
yuukun <beslot.sam@orange.fr>
2021-01-27 13:51:15 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import apiKey from '../apiKey'
 
 export default function Home() {
+
+
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch('http://newsapi.org/v2/top-headlines?' + 'country=fr&' + `apiKey=${apiKey}`)
+      const newNewsList = await response.json()
+      setNews(newNewsList.articles)
+      console.log(news)
+    }
+
+    loadData()
+  }, [])
+
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -20,33 +38,17 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {news.map((e, index) => (
+            <div key={index}>
+              {/* <Link href="/"> */}
+                <a href="https://nextjs.org/docs" className={styles.card}>
+                <h3>{e.title}</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+                </a>
+              {/* </Link> */}
+            </div>
+          ))}
         </div>
       </main>
 
