be177811e50ecc54a1b19b28c4743764cc1afae9
yuukun <beslot.sam@orange.fr>
2021-01-29 18:10:25 +0100

    final push

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,89 +1,89 @@
-// import { useState, useEffect } from 'react'
-// import React from 'react'
-// import apiKey from '../../apiKey'
-// import 'antd/dist/antd.css'
-// import { withRouter } from 'next/router'
-// import { Breadcrumb, Divider, PageHeader, Descriptions, Image, Layout } from 'antd'
-// import moment from 'moment'
+import { useState, useEffect } from 'react'
+import React from 'react'
+import apiKey from '../../apiKey'
+import 'antd/dist/antd.css'
+import { withRouter } from 'next/router'
+import { Breadcrumb, Divider, PageHeader, Descriptions, Image, Layout } from 'antd'
+import moment from 'moment'
 
-// function Details({ router: { query } }) {
+function Details(/*{ router: { query } }*/) {
 
-//     const id = query.id
-//     console.log(query.object)
-//     const [article, setArticle] = useState(JSON.parse(
-//         {
+    // const id = query.id
+    // console.log(query.object)
+    // const [article, setArticle] = useState(JSON.parse(
+    //     {
            
-//             "author":"Auteur",
-//             "title":"Titre",
-//             "description":"Description",
-//             "url":"urlArticle",
-//             "urlToImage":"UrlImage",
-//             "publishedAt":"DatePublication",
-//             "content":"Contenu", 
-//             "source":
-//             {
-//                 "id": null,
-//                 "name": "Publicateur"
-//             },
-//         }
-//     ))
-//     useEffect(() => {
-//       async function loadData() {
-//         const response = await fetch('http://newsapi.org/v2/top-headlines?' + 'country=fr&' + `apiKey=${apiKey}`)
-//         const newArticleList = await response.json()
-//         let currentArticle = newArticleList.articles[id]
-//         let articleContentPlus = currentArticle.content.split('…')
-//         currentArticle.content = articleContentPlus[articleContentPlus.length - 2]
-//         setArticle(currentArticle)
-//       }
+    //         "author":"Auteur",
+    //         "title":"Titre",
+    //         "description":"Description",
+    //         "url":"urlArticle",
+    //         "urlToImage":"UrlImage",
+    //         "publishedAt":"DatePublication",
+    //         "content":"Contenu", 
+    //         "source":
+    //         {
+    //             "id": null,
+    //             "name": "Publicateur"
+    //         },
+    //     }
+    // ))
+    // useEffect(() => {
+    //   async function loadData() {
+    //     const response = await fetch('http://newsapi.org/v2/top-headlines?' + 'country=fr&' + `apiKey=${apiKey}`)
+    //     const newArticleList = await response.json()
+    //     let currentArticle = newArticleList.articles[id]
+    //     let articleContentPlus = currentArticle.content.split('…')
+    //     currentArticle.content = articleContentPlus[articleContentPlus.length - 2]
+    //     setArticle(currentArticle)
+    //   }
   
-//       loadData()
-//     })
+    //   loadData()
+    // })
 
-//     const { Footer } = Layout;
+    // const { Footer } = Layout;
 
-//     return (
-//         <div className="site-card-wrapper">
-//             <Breadcrumb style={{ padding: "1%", backgroundColor: '#87CEFA' }}>
-//                 <Breadcrumb.Item> </Breadcrumb.Item>
-//                 <Breadcrumb.Item>
-//                     <a href="/">Home</a>
-//                 </Breadcrumb.Item>
-//             </Breadcrumb>
-//             <div className="site-page-header-ghost-wrapper">
-//             <Divider style={{marginTop: 0}}/>
-//                 <PageHeader
-//                     ghost={false}
-//                     title={article.title || "Titre"}
-//                 >
-//                     <Descriptions size="middle" column={1}>
-//                         <Descriptions.Item>
-//                             <div>{article.description || "Description"}</div>
-//                         </Descriptions.Item >
-//                         <Descriptions.Item id="imageArticle">
-//                             <Image src={article.urlToImage || "urlImage"} alt="Image Non Disponible"/>
-//                         </Descriptions.Item>
-//                         <Descriptions.Item>
-//                             <div style={{ fontSize: '1.2em' }}>{article.content || "Contenu"}...<a href={article.url || "urlArticle"}>[Voir plus ici]</a></div>
-//                         </Descriptions.Item>
-//                         <Descriptions.Item>
-//                             <div style={{ fontStyle: 'italic' }}>{article.source.name || "Publicateur"} - {moment(article.publishedAt || "DateDePublication").format("DD-MM-YYYY HH:mm")}</div>
-//                         </Descriptions.Item>
-//                     </Descriptions>
-//                 </PageHeader>
-//                 <Divider/>
-//                 <Footer id="footerArticle">
-//                     <a
-//                         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                     >
-//                         Powered by{' '}
-//                         <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-//                     </a></Footer>
-//             </div>
-//         </div>
-//     )
-// }
+    return (
+        <div className="site-card-wrapper">
+            <Breadcrumb style={{ padding: "1%", backgroundColor: '#87CEFA' }}>
+                <Breadcrumb.Item> </Breadcrumb.Item>
+                <Breadcrumb.Item>
+                    <a href="/">Home</a>
+                </Breadcrumb.Item>
+            </Breadcrumb>
+            {/* <div className="site-page-header-ghost-wrapper">
+            <Divider style={{marginTop: 0}}/>
+                <PageHeader
+                    ghost={false}
+                    title={article.title || "Titre"}
+                >
+                    <Descriptions size="middle" column={1}>
+                        <Descriptions.Item>
+                            <div>{article.description || "Description"}</div>
+                        </Descriptions.Item >
+                        <Descriptions.Item id="imageArticle">
+                            <Image src={article.urlToImage || "urlImage"} alt="Image Non Disponible"/>
+                        </Descriptions.Item>
+                        <Descriptions.Item>
+                            <div style={{ fontSize: '1.2em' }}>{article.content || "Contenu"}...<a href={article.url || "urlArticle"}>[Voir plus ici]</a></div>
+                        </Descriptions.Item>
+                        <Descriptions.Item>
+                            <div style={{ fontStyle: 'italic' }}>{article.source.name || "Publicateur"} - {moment(article.publishedAt || "DateDePublication").format("DD-MM-YYYY HH:mm")}</div>
+                        </Descriptions.Item>
+                    </Descriptions>
+                </PageHeader>
+                <Divider/>
+                <Footer id="footerArticle">
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{' '}
+                        <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+                    </a></Footer>
+            </div> */}
+        </div>
+    )
+}
 
-// export default withRouter(Details)
+export default withRouter(Details)
